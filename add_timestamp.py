1c635f2a9bc4c2554c2424ee74ede994b31f8125
ahmed <a.elsayed.it@gmail.com>
2023-11-05 15:17:26 +0200

    Add font with asstes

--- add_timestamp.py
+++ add_timestamp.py
@@ -15,14 +15,28 @@ def get_image_datetime_original(image_path):
     except (AttributeError, KeyError, IndexError):
         return None
 
-def add_timestamp_to_image(image_path, output_path, timestamp):
+def add_text_with_scaled_font(image_path, output_path, timestamp, custom_font_path, padding_ratio=0.5):
     pil_image = Image.open(image_path)
     draw = ImageDraw.Draw(pil_image)
 
-    font = ImageFont.load_default()
-    font_size = 24
-    text_color = (255, 255, 255)
-    position = (pil_image.width - 120, pil_image.height - 40)  # Adjust the position as needed
+    image_width, image_height = pil_image.size
+
+    # Calculate the font size based on image resolution and padding
+    padding_x = int(image_width * padding_ratio)
+    padding_y = int(image_height * padding_ratio)
+    max_dimension = max(image_width - 2 * padding_x, image_height - 2 * padding_y)
+    custom_font_size = int(max_dimension * 0.02)  # Adjust font size as needed
+
+    # Specify the custom font
+    custom_font = ImageFont.truetype(custom_font_path, custom_font_size)
+
+    # Ensure the output folder exists
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+
+    # Calculate the position with padding
+    position_x = image_width - custom_font.getsize(timestamp)[0] - 1 * custom_font_size
+    position_y = image_height - custom_font.getsize(timestamp)[1] - 1 * custom_font_size
+    position = (position_x, position_y)
 
     # Split the timestamp string into date and time components
     date_str, time_str = timestamp.split(' ')
@@ -40,10 +54,10 @@ def add_timestamp_to_image(image_path, output_path, timestamp):
     # Format the datetime object as a string
     timestamp_str = original_datetime.strftime('%d/%m/%Y %H:%M')
 
-    # Ensure the output folder exists
-    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    text_color = (255, 255, 255)
 
-    draw.text(position, timestamp_str, fill=text_color, font=font)
+    # Draw the text with the specified font and position
+    draw.text(position, timestamp_str, fill=text_color, font=custom_font)
     pil_image.save(output_path)
 
 # Specify the folder containing the images
@@ -52,16 +66,22 @@ folder_path = 'images/'  # Change this to your image folder
 # Specify the output folder for the modified images
 output_folder = 'output_images/'
 
+# Specify the path to your custom font file
+custom_font_path = 'arial.ttf'
+
 # Loop through the files in the folder
 for filename in os.listdir(folder_path):
-    if filename.endswith(('.jpg', '.jpeg', '.png', '.gif')):
+    if filename.lower().endswith(('.jpg', '.jpeg', '.png', '.gif')):
         image_path = os.path.join(folder_path, filename)
         timestamp = get_image_datetime_original(image_path)
         if timestamp:
             timestamp_str = timestamp  # Assuming your timestamp is in the correct format
-            output_path = os.path.join(output_folder, f"{filename}")
-            
-            add_timestamp_to_image(image_path, output_path, timestamp_str)
-            print(f"Added timestamp to {filename} and saved to {output_path}")
+            output_path = os.path.join(output_folder, filename)
 
-print("Timestamps added to all eligible images in the folder and saved in the output folder.")
+            # Specify the padding ratio (e.g., 0.05 for 5% padding)
+            padding_ratio = 0.025
+
+            add_text_with_scaled_font(image_path, output_path, timestamp_str, custom_font_path, padding_ratio)
+            print(f"Added text with scaled font and padding to {filename} and saved to {output_path}")
+
+print("Text with scaled font and padding added to all eligible images in the folder and saved in the output folder.")
